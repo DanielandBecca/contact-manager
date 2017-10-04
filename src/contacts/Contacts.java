5428ec36789f139bc2d6c56020da3bd150f27cc3
danielstrolle <danielstrolle@gmail.com>
2017-10-04 14:04:46 -0500

    Create a main method in our Contacts class
    * Create an instance of our FileHandler class, run our createDirectory and createFile method...makes sure we have the right directory and file.
    
    * move the printMenu method to our Contacts class, we only use it here

--- src/contacts/Contacts.java
+++ src/contacts/Contacts.java
@@ -1,4 +1,32 @@
 package contacts;
 
+import util.FileHandler;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Contacts {
+
+    public static void main(String[] args) {
+        FileHandler fh = new FileHandler("contacts", "contacts.txt");
+        try {
+            fh.createDirectory();
+            fh.createFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printMenu();
+    }
+
+    public static void printMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. View Contacts");
+        System.out.println("2. Add new contact");
+        System.out.println("3. Search a contact by name");
+        System.out.println("4. Delete an existing contact");
+        System.out.println("5. Exit");
+        System.out.println("Enter an option(1, 2, 3, 4, 5): ");
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+    }
 }
